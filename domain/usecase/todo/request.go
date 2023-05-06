f09d912dc4802c4abfd1da79ea5c68e728f23d87
snozaki <snozaki@netprotections.co.jp>
2023-05-06 15:50:05 +0900

    コミット漏れ

---
+++ domain/usecase/todo/request.go
@@ -0,0 +1,13 @@
+package todo
+
+type Request struct {
+	Id          string
+	UserId      string
+	Title       string
+	Description string
+	State       string
+	CreatedAt   string
+	CreatedBy   string
+	UpdatedAt   string
+	UpdatedBy   string
+}
